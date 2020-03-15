4d0b5e06bff317a5186926a01355cb49b3dcf7a6
oscar3x39 <3096182+oscar3x39@users.noreply.github.com>
2020-03-15 17:39:57 +0800

    (feature): Add 1108

---
+++ algorithms/php/defanging-an-ip-address.php
@@ -0,0 +1,12 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String $address
+     * @return String
+     */
+    function defangIPaddr($address) {
+        return preg_replace("/\./", "[.]", $address);
+    }
+}
